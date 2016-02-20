0a34ce7a498fb7360654ae240ac89501cd1090a4
aykutakin <aykutakin90@gmail.com>
2016-02-20 21:51:03 +0200

    Adds repository annotation to languageRepository

--- web/src/main/java/com/dedeler/tweetguess/repository/LanguageRepository.java
+++ web/src/main/java/com/dedeler/tweetguess/repository/LanguageRepository.java
@@ -2,9 +2,11 @@ package com.dedeler.tweetguess.repository;
 
 import com.dedeler.tweetguess.model.Language;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Aykut on 20.02.2016.
  */
+@Repository
 public interface LanguageRepository extends CrudRepository<Language, String> {
 }
