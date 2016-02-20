5b475d947742e15ed654cf7e222c48e5635d0b74
aykutakin <aykutakin90@gmail.com>
2016-02-20 16:04:41 +0200

    Adds twitter api sample

--- batch/src/main/java/com/dedeler/tweetguess/AppRunner.java
+++ batch/src/main/java/com/dedeler/tweetguess/AppRunner.java
@@ -1,8 +1,13 @@
 package com.dedeler.tweetguess;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+import twitter4j.Category;
+import twitter4j.TweetGuessTwitter;
+import twitter4j.User;
+import twitter4j.api.HelpResources;
 
 /**
  * Created by Aykut on 20.02.2016.
@@ -10,9 +15,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppRunner implements ApplicationRunner {
 
+    @Autowired
+    private TweetGuessTwitter tweetGuessTwitter;
+
     @Override
     public void run(ApplicationArguments applicationArguments) throws Exception {
-
+        for(HelpResources.Language language : tweetGuessTwitter.getLanguages()) {
+            for(Category category : tweetGuessTwitter.getSuggestedUserCategories(language.getCode())) {
+                for(User user : tweetGuessTwitter.getUserSuggestions(category.getSlug(), language.getCode())) {
+                    System.out.println(user.getName());
+                }
+            }
+        }
     }
 
 }
---
+++ batch/src/main/java/com/dedeler/tweetguess/config/TwitterConfig.java
@@ -0,0 +1,43 @@
+package com.dedeler.tweetguess.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import twitter4j.TweetGuessTwitter;
+import twitter4j.TweetGuessTwitterImpl;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AuthorizationFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+/**
+ * Created by Aykut on 20.02.2016.
+ */
+@Configuration
+public class TwitterConfig {
+
+    @Value("${twitter.consumer-key}")
+    private String consumerKey;
+
+    @Value("${twitter.consumer-secret}")
+    private String consumerSecret;
+
+    @Value("${twitter.access-token}")
+    private String accessToken;
+
+    @Value("${twitter.access-token-secret}")
+    private String accessTokenSecret;
+
+    @Bean
+    public TweetGuessTwitter tweetGuessTwitter() {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey(consumerKey)
+                .setOAuthConsumerSecret(consumerSecret)
+                .setOAuthAccessToken(accessToken)
+                .setOAuthAccessTokenSecret(accessTokenSecret);
+        twitter4j.conf.Configuration configuration = cb.build();
+        return new TweetGuessTwitterImpl(configuration, AuthorizationFactory.getInstance(configuration));
+    }
+
+}
