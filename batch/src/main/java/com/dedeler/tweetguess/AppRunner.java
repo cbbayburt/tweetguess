ba6035193ca4c19f35609e68ce776067caf56da2
aykutakin <aykutakin90@gmail.com>
2016-02-21 02:25:58 +0200

    Adds excetion handling for batch process

--- batch/src/main/java/com/dedeler/tweetguess/AppRunner.java
+++ batch/src/main/java/com/dedeler/tweetguess/AppRunner.java
@@ -1,5 +1,6 @@
 package com.dedeler.tweetguess;
 
+import com.dedeler.tweetguess.model.Category;
 import com.dedeler.tweetguess.model.Language;
 import com.dedeler.tweetguess.model.Person;
 import com.dedeler.tweetguess.model.Tweet;
@@ -8,20 +9,26 @@ import com.dedeler.tweetguess.repository.LanguageRepository;
 import com.dedeler.tweetguess.repository.PersonRepository;
 import com.dedeler.tweetguess.repository.TweetRepository;
 import org.dozer.Mapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-import twitter4j.*;
+import twitter4j.Status;
+import twitter4j.TweetGuessTwitter;
+import twitter4j.TwitterException;
+import twitter4j.User;
 import twitter4j.api.HelpResources;
 
-import javax.transaction.Transactional;
 
 /**
  * Created by Aykut on 20.02.2016.
  */
 @Component
 public class AppRunner implements ApplicationRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppRunner.class);
 
     @Autowired
     private TweetGuessTwitter tweetGuessTwitter;
@@ -42,34 +49,93 @@ public class AppRunner implements ApplicationRunner {
     private TweetRepository tweetRepository;
 
     @Override
-    @Transactional
     public void run(ApplicationArguments applicationArguments) throws Exception {
-        for(HelpResources.Language tgtLanguage : tweetGuessTwitter.getLanguages()) {
-            Language language = dozerBeanMapper.map(tgtLanguage, Language.class);
-            languageRepository.save(language);
-            for(Category tgtCategory : tweetGuessTwitter.getSuggestedUserCategories(language.getCode())) {
-                com.dedeler.tweetguess.model.Category category = dozerBeanMapper.map(tgtCategory, com.dedeler.tweetguess.model.Category.class);
-                category.setLanguageId(language.getCode());
-                categoryRepository.save(category);
-                for(User tgtUser : tweetGuessTwitter.getUserSuggestions(category.getSlug(), language.getCode())) {
+        handleLanguage();
+    }
+
+    private void handleLanguage() {
+        boolean exceptionStatus = false;
+        do {
+            try {
+                for(HelpResources.Language tgtLanguage : tweetGuessTwitter.getLanguages()) {
+                    exceptionStatus = false;
+                    Language language = dozerBeanMapper.map(tgtLanguage, Language.class);
+                    languageRepository.save(language);
+                    handleCategory(language);
+                }
+            } catch(TwitterException e) {
+                exceptionStatus = true;
+                LOGGER.debug("Error in handleLanguage");
+                sleep();
+            }
+        } while(exceptionStatus);
+    }
+
+    private void handleCategory(Language language) {
+        boolean exceptionStatus = false;
+        do {
+            try {
+                for(twitter4j.Category tgtCategory : tweetGuessTwitter.getSuggestedUserCategories(language.getCode())) {
+                    exceptionStatus = false;
+                    Category category = dozerBeanMapper.map(tgtCategory, Category.class);
+                    category.setLanguageId(language.getCode());
+                    categoryRepository.save(category);
+                    handlePerson(category);
+                }
+            } catch(TwitterException e) {
+                exceptionStatus = true;
+                LOGGER.debug("Error in handleCategory");
+                sleep();
+            }
+        } while(exceptionStatus);
+    }
+
+    private void handlePerson(Category category) {
+        boolean exceptionStatus = false;
+        do {
+            try {
+                for(User tgtUser : tweetGuessTwitter.getUserSuggestions(category.getSlug(), category.getLanguageId())) {
+                    exceptionStatus = false;
                     Person person = dozerBeanMapper.map(tgtUser, Person.class);
                     person.setCategoryId(category.getSlug());
-                    person.setLangId(language.getCode());
+                    person.setLangId(category.getLanguageId());
                     personRepository.save(person);
-//                    Paging paging = new Paging();
-//                    paging.count(200);
-//                    for(int i=0; i<5; i++) {
-//                        paging.setPage(i);
-                        for(Status tgtStatus : tweetGuessTwitter.getUserTimeline(person.getId())) {
-                            Tweet tweet = dozerBeanMapper.map(tgtStatus, Tweet.class);
-                            tweet.setCategoryId(category.getSlug());
-                            tweet.setLangId(language.getCode());
-                            tweet.setPersonId(person.getId());
-                            tweetRepository.save(tweet);
-                        }
-//                    }
+                    handleTweet(person);
                 }
+            } catch(TwitterException e) {
+                exceptionStatus = true;
+                LOGGER.debug("Error in handlePerson");
+                sleep();
             }
+        } while(exceptionStatus);
+    }
+
+    private void handleTweet(Person person) {
+        boolean exceptionStatus = false;
+        do {
+            try {
+                for(Status tgtStatus : tweetGuessTwitter.getUserTimeline(person.getId())) {
+                    exceptionStatus = false;
+                    Tweet tweet = dozerBeanMapper.map(tgtStatus, Tweet.class);
+                    tweet.setCategoryId(person.getCategoryId());
+                    tweet.setLangId(person.getLangId());
+                    tweet.setPersonId(person.getId());
+                    tweetRepository.save(tweet);
+                }
+            } catch(TwitterException e) {
+                exceptionStatus = true;
+                LOGGER.debug("Error in handleTweet");
+                sleep();
+            }
+        } while(exceptionStatus);
+    }
+
+    private void sleep() {
+        try {
+            //Sleep 5 minutes
+            Thread.sleep(5*60*1000);
+        } catch (InterruptedException ie) {
+            LOGGER.error("Error in sleep");
         }
     }
 
