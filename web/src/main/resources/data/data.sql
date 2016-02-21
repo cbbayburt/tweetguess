abe08886078692385e1bdf7fbf871cafd4410473
aykutakin <aykutakin90@gmail.com>
2016-02-21 02:25:19 +0200

    Removes data.sql

--- web/src/main/resources/data/data.sql
+++
@@ -1,4 +0,0 @@
-insert into language(code, name, status) values ('en', 'English', 'Active');
-insert into category(slug, name, size, language_id) values ('music', 'Music', 10, 'en');
-insert into category(slug, name, size, language_id) values ('sport', 'Sports', 10, 'en');
-commit;
