a58fceff7ca8e32bf2e3930e1cd6a4adb8cd0e17
aykutakin <aykutakin90@gmail.com>
2016-02-22 01:32:09 +0200

    Adds new process for h2 mem db

---
+++ db/src/main/java/com/dedeler/tweetguess/AppRunner.java
@@ -0,0 +1,19 @@
+package com.dedeler.tweetguess;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * Created by Aykut on 20.02.2016.
+ */
+@Component
+public class AppRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+        while(true);
+    }
+
+}
