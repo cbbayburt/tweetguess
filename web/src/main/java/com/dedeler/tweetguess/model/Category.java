378854ff8d84518bf975a4d2ef340efb19bc8555
aykutakin <aykutakin90@gmail.com>
2016-02-21 23:29:30 +0200

    Adds auto generate id

--- web/src/main/java/com/dedeler/tweetguess/model/Category.java
+++ web/src/main/java/com/dedeler/tweetguess/model/Category.java
@@ -5,10 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 /**
  * Created by Aykut on 20.02.2016.
@@ -21,6 +18,7 @@ import javax.persistence.ManyToOne;
 public class Category {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     private String slug;
