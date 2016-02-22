a59488d7db452f4ec707e3e9a3edda778aa386cb
aykutakin <aykutakin90@gmail.com>
2016-02-22 04:00:06 +0200

    Changes for valid flag

--- web/src/main/resources/data/data.sql
+++ web/src/main/resources/data/data.sql
@@ -34,33 +34,33 @@ insert into person (id, name, screen_Name, profile_imageurl, url, is_Protected, 
   values (237448184, 'Juergen Hoeller', 'juergen', 'http://pbs.twimg.com/profile_images/378800000863144334/uZWGPcJz_normal.jpeg', 'http://t.co/btPeg0c2dr', 'false', 1890, 225, 115, 'en', 1);
 insert into person (id, name, screen_Name, profile_imageurl, url, is_Protected, followers_Count, friends_Count, favourites_Count, language_Id, category_Id)
   values (237448185, 'Steve Jobs', 'steve', 'http://pbs.twimg.com/profile_images/378800000863144334/uZWGPcJz_normal.jpeg', 'http://t.co/btPeg0c2dr', 'false', 1890, 225, 115, 'en', 1);
-insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id)
-  values (1, null, 'tweet1', 100, 200, 'en', 1, 237448180);
-insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id)
-  values (2, null, 'tweet2', 100, 200, 'en', 1, 237448180);
-insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id)
-  values (3, null, 'tweet3', 100, 200, 'en', 1, 237448180);
-insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id)
-  values (4, null, 'tweet4', 100, 200, 'en', 1, 237448180);
-insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id)
-  values (5, null, 'tweet5', 100, 200, 'en', 1, 237448181);
-insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id)
-  values (6, null, 'tweet6', 100, 200, 'en', 1, 237448181);
-insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id)
-  values (7, null, 'tweet7', 100, 200, 'en', 1, 237448181);
-insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id)
-  values (8, null, 'tweet8', 100, 200, 'en', 1, 237448182);
-insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id)
-  values (9, null, 'tweet9', 100, 200, 'en', 1, 237448182);
-insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id)
-  values (10, null, 'tweet10', 100, 200, 'en', 1, 237448182);
-insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id)
-  values (11, null, 'tweet11', 100, 200, 'en', 1, 237448182);
-insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id)
-  values (12, null, 'tweet12', 100, 200, 'en', 1, 237448182);
-insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id)
-  values (13, null, 'tweet13', 100, 200, 'en', 1, 237448183);
-insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id)
-  values (14, null, 'tweet14', 100, 200, 'en', 1, 237448183);
+insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id, valid)
+  values (1, null, 'tweet1', 100, 200, 'en', 1, 237448180, true);
+insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id, valid)
+  values (2, null, 'tweet2', 100, 200, 'en', 1, 237448180, true);
+insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id, valid)
+  values (3, null, 'tweet3', 100, 200, 'en', 1, 237448180, true);
+insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id, valid)
+  values (4, null, 'tweet4', 100, 200, 'en', 1, 237448180, true);
+insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id, valid)
+  values (5, null, 'tweet5', 100, 200, 'en', 1, 237448181, true);
+insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id, valid)
+  values (6, null, 'tweet6', 100, 200, 'en', 1, 237448181, true);
+insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id, valid)
+  values (7, null, 'tweet7', 100, 200, 'en', 1, 237448181, true);
+insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id, valid)
+  values (8, null, 'tweet8', 100, 200, 'en', 1, 237448182, true);
+insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id, valid)
+  values (9, null, 'tweet9', 100, 200, 'en', 1, 237448182, true);
+insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id, valid)
+  values (10, null, 'tweet10', 100, 200, 'en', 1, 237448182, true);
+insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id, valid)
+  values (11, null, 'tweet11', 100, 200, 'en', 1, 237448182, true);
+insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id, valid)
+  values (12, null, 'tweet12', 100, 200, 'en', 1, 237448182, true);
+insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id, valid)
+  values (13, null, 'tweet13', 100, 200, 'en', 1, 237448183, true);
+insert into tweet (id, created_At, text, favorite_Count, retweet_Count, language_Id, category_Id, person_Id, valid)
+  values (14, null, 'tweet14', 100, 200, 'en', 1, 237448183, true);
 
 commit;
