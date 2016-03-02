3daec0e7e6faa6c0253a6d9a7018bc7417407d87
aykutakin <aykutakin90@gmail.com>
2016-03-02 19:58:56 +0200

    Changes db create script

--- web/db/db.sql
+++ web/db/db.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS tweetguessdb;
+
 CREATE DATABASE tweetguessdb CHARACTER SET utf8mb4;
 
-GRANT ALL ON tweetguess.* TO 'tweetguess' IDENTIFIED BY 'tweetguess';
+GRANT ALL ON tweetguessdb.* TO 'tweetguess' IDENTIFIED BY 'tweetguess';
