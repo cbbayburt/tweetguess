53e07dc73380a939c54ce0a1f503869ac02eb762
aykutakin <aykutakin90@gmail.com>
2016-03-06 23:07:52 +0200

    Changes for mysql case sensitivity

--- web/src/main/java/com/dedeler/tweetguess/repository/TweetRepository.java
+++ web/src/main/java/com/dedeler/tweetguess/repository/TweetRepository.java
@@ -13,13 +13,13 @@ import java.util.List;
 @Repository
 public interface TweetRepository extends CrudRepository<Tweet, Long> {
 
-    @Query(value = "select count(*) from Tweet t inner join Person p on t.person_id = p.id inner join Category_Person_Lk cplk on p.id = cplk.person_id " +
-            "inner join Category g on cplk.language_id = g.language_id and cplk.slug = g.slug where g.slug = ?1 and g.language_id = ?2 and t.valid = ?3", nativeQuery = true)
+    @Query(value = "select count(*) from tweet t inner join person p on t.person_id = p.id inner join category_person_lk cplk on p.id = cplk.person_id " +
+            "inner join category g on cplk.language_id = g.language_id and cplk.slug = g.slug where g.slug = ?1 and g.language_id = ?2 and t.valid = ?3", nativeQuery = true)
     Integer getTweetCountByCategoryAndValid(String slug, String language, Boolean valid);
 
-    @Query(value = "select * from (select @rowid\\:=@rowid+1 as row_num, t.* from Tweet t inner join Person p on t.person_id = p.id " +
-            "inner join Category_Person_Lk cplk on p.id = cplk.person_id inner join Category g on cplk.language_id = g.language_id and cplk.slug = g.slug " +
-            "left outer join (select q.tweet_id, q.game_id from Question q inner join Game g on q.game_id = g.id where g.user_id = ?3) q on t.id = q.tweet_id " +
+    @Query(value = "select * from (select @rowid\\:=@rowid+1 as row_num, t.* from tweet t inner join person p on t.person_id = p.id " +
+            "inner join category_person_lk cplk on p.id = cplk.person_id inner join category g on cplk.language_id = g.language_id and cplk.slug = g.slug " +
+            "left outer join (select q.tweet_id, q.game_id from question q inner join game g on q.game_id = g.id where g.user_id = ?3) q on t.id = q.tweet_id " +
             "where g.slug = ?1 and g.language_id = ?2 and t.valid = ?5 order by q.game_id asc, t.created_at desc) as main, (SELECT @rowid\\:=0) as init " +
             "where row_num in ?4", nativeQuery = true)
     List<Tweet> getGeoRandomizedTweetsByCategoryAndValid(String slug, String language, String userId, List<Integer> randomNumberList, Boolean valid);
